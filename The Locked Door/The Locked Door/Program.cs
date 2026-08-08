cf5a707ac645402604d7d344f5616eb382755ad4
coltonwiggs <90272823+coltonwiggs@users.noreply.github.com>
2026-08-08 18:53:34 -0500

    The Locked Door

---
+++ The Locked Door/The Locked Door/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+public class Door
+{
+    private int _startingCode;
+    public DoorState State { get; private set; }
+    
+    public Door(int startingCode)
+    {
+        _startingCode = startingCode;
+        State = DoorState.Closed;
+    }
+    
+    public void Lock()
+    {
+        if (State == DoorState.Closed)
+            State = DoorState.Locked;
+    }
+
+    public void Open()
+    {
+        if (State == DoorState.Closed)
+            State = DoorState.Open;
+    }
+
+    public void Close()
+    {
+        if (State == DoorState.Open)
+            State = DoorState.Closed;
+    }
+
+    public void Unlock(int passcode)
+    {
+        if (State == DoorState.Locked && _startingCode == passcode)
+            State = DoorState.Closed;
+    }
+}
+
+public enum DoorState { Open, Closed, Locked }

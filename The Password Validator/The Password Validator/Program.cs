d27d72ff291d260cc19ebe82dad63bbadc5e64a9
coltonwiggs <90272823+coltonwiggs@users.noreply.github.com>
2026-08-08 18:54:53 -0500

    The Password Validator

---
+++ The Password Validator/The Password Validator/Program.cs
@@ -0,0 +1,35 @@
+﻿while (true)
+{
+    Console.Write("Enter password: ");
+    string enteredPassword = Console.ReadLine();
+    PasswordValidator passwordValidator = new PasswordValidator(enteredPassword);
+    Console.WriteLine(passwordValidator.AnalysePassword(enteredPassword));
+}
+
+public class PasswordValidator
+{
+    private string _password;
+
+    public PasswordValidator(string password)
+    {
+        _password = password;
+    }
+
+    public string AnalysePassword(string password)
+    {
+        if (password.Length < 6 || password.Length > 13)
+            return ("Invalid password.");
+
+        else if (password.Count(char.IsUpper) < 1 || password.Count(char.IsLower) < 1)
+            return ("Invalid password.");
+
+        else if (password.Count(char.IsNumber) < 1)
+            return ("Invalid password.");
+        else if (password.Contains('T'))
+            return ("Invalid password.");
+        else if (password.Contains('&'))
+            return ("Invalid password.");
+        else
+            return ("Valid password.");      
+    }
+}

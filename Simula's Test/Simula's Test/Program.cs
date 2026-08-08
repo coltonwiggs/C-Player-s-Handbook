65640e5af9e044cab3e5df0ba896625b90394b83
coltonwiggs <90272823+coltonwiggs@users.noreply.github.com>
2026-08-08 18:48:54 -0500

    Simula's Test

---
+++ Simula's Test/Simula's Test/Program.cs
@@ -0,0 +1,19 @@
+﻿ChestState currentState = ChestState.Open;
+
+while (true)
+{
+    Console.Write($"The chest is {currentState}. What do you want to do? ");
+
+    string input = Console.ReadLine();
+    
+    if (currentState == ChestState.Open && input == "close")
+        currentState = ChestState.Closed;
+    if (currentState == ChestState.Closed && input == "lock")
+        currentState = ChestState.Locked;
+    if (currentState == ChestState.Locked && input == "unlock")
+        currentState = ChestState.Closed;
+    if (currentState == ChestState.Closed && input == "open")
+        currentState = ChestState.Open;   
+}
+
+enum ChestState { Open, Closed, Locked }

8638a7c4921456e6bdb6d97961a231816b0178ca
coltonwiggs <90272823+coltonwiggs@users.noreply.github.com>
2026-08-11 21:52:59 -0500

    Add files via upload

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -0,0 +1,22 @@
+﻿TicTacToe game = new TicTacToe()
+
+class TicTacToe
+{
+    private char _playerChoice;
+
+    public TicTacToe(char playerChoice)
+    {
+        _playerChoice = playerChoice;
+    }
+
+    public char[,] gameBoard = new char[3, 3];
+    
+    public static void DrawGameBoard(char[] gameBoard)
+    {
+        Console.WriteLine($" {gameBoard[0]} | {gameBoard[1]} | {gameBoard[2]} ");
+        Console.WriteLine("---+---+---");
+        Console.WriteLine($" {gameBoard[3]} | {gameBoard[4]} | {gameBoard[5]} ");
+        Console.WriteLine("---+---+---");
+        Console.WriteLine($" {gameBoard[6]} | {gameBoard[7]} | {gameBoard[8]} ");
+    }
+}

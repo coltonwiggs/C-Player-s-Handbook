ac5e7ab154e626faa8ece70966dae76f00d0d7e4
coltonwiggs <90272823+coltonwiggs@users.noreply.github.com>
2026-08-13 23:37:09 -0500

    Add files via upload
    Working Tic-Tac-Toe program

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -1,19 +1,37 @@
 ﻿Console.WriteLine("Welcome to Tic-Tac-Toe!");
 
-char[, ] gameBoard = new char[3, 3];
-
-while (true)
+char[, ] gameBoard = new char[3, 3]
 {
-    for (int i = 0; i < 9; i++)
+    { '7', '8', '9' },
+    { '4', '5', '6' },
+    { '1', '2', '3' }
+};
+
+for (int i = 0; i < 9; i++)
+{
+    Console.WriteLine($"It is {GetPlayerTurn(i)}'s turn.");
+
+    PrintBoard(gameBoard);
+
+    int choice = GetPlayerChoice(gameBoard);
+
+    UpdateBoard(gameBoard, choice, GetPlayerTurn(i));
+
+    if (CheckForWin(gameBoard) == true && i < 9)
     {
-        Console.WriteLine($"It is {GetPlayerTurn(i)}'s turn.");
-
+        Console.Clear();
+        Console.WriteLine($"{GetPlayerTurn(i)}'s win!!!");
         PrintBoard(gameBoard);
-
-        int choice = GetPlayerChoice();
-
-        UpdateBoard(gameBoard, choice, GetPlayerTurn(i));
+        break;
     }
+    else if (i == 8)
+    {
+        Console.Clear();
+        Console.WriteLine("It's a tie :(");
+        PrintBoard(gameBoard);
+    }
+    else
+        Console.Clear();
 }
 
 char GetPlayerTurn(int round)
@@ -23,43 +41,106 @@ char GetPlayerTurn(int round)
     if (round % 2 == 0)
         player = 'X';
     else
-        player = 'Y';
+        player = 'O';
     return player;
 }
 
-int GetPlayerChoice()
+int GetPlayerChoice(char[, ] currentBoard)
 {
-    Console.Write("What square do you want to play in? ");
-    int choice = Convert.ToInt32(Console.ReadLine());
-    while (choice < 1 || choice > 9)
+    int choice;
+    bool isValidchoice = false;
+    
+    do 
     {
-        Console.Write("Invalid choice. Try Again. ");
+        Console.Write("What square do you want to play in? ");
         choice = Convert.ToInt32(Console.ReadLine());
+
+        if (choice < 1 || choice > 9)
+        {
+            Console.Write("Invalid choice. Try Again. ");
+            continue;
+        }
+
+        int row = 2 - (choice - 1) / 3;
+        int col = (choice - 1) % 3;
+
+        if (currentBoard[row, col] == 'X' || currentBoard[row, col] == 'O')
+        {
+            Console.WriteLine("Invalid choice. Try Again. ");
+        }
+        else
+        {
+            isValidchoice = true;
+        }
     }
+    while (!isValidchoice);
+
     return choice;
 }
 
 void PrintBoard(char[, ] currentBoard)
 {
-    Console.WriteLine($" {currentBoard[0, 0]}  | {currentBoard[0, 1]}  |  {currentBoard[0, 2]} ");
+    Console.WriteLine($" {currentBoard[0, 0]} | {currentBoard[0, 1]} | {currentBoard[0, 2]} ");
     Console.WriteLine("---+---+---");
-    Console.WriteLine($" {currentBoard[1, 0]}  | {currentBoard[1, 1]}  |  {currentBoard[1, 2]} ");
+    Console.WriteLine($" {currentBoard[1, 0]} | {currentBoard[1, 1]} | {currentBoard[1, 2]} ");
     Console.WriteLine("---+---+---");
-    Console.WriteLine($" {currentBoard[2, 0]}  | {currentBoard[2, 1]}  |  {currentBoard[2, 2]} ");
+    Console.WriteLine($" {currentBoard[2, 0]} | {currentBoard[2, 1]} | {currentBoard[2, 2]} ");
 }
 
 char[, ] UpdateBoard(char[, ] currentBoard, int playerChoice, char playerTurn)
 {
-    
     if (playerChoice == 1 || playerChoice == 2 || playerChoice == 3)
     {
-        currentBoard[0, playerChoice] = playerTurn;
+        playerChoice = playerChoice - 1;
+        currentBoard[2, playerChoice] = playerTurn;
     }
     else if (playerChoice == 4 || playerChoice == 5 || playerChoice == 6)
     {
+        playerChoice = playerChoice - 4;
         currentBoard[1, playerChoice] = playerTurn;
     }
     else
-        currentBoard[2, playerChoice] = playerTurn;
+    {
+        playerChoice = playerChoice - 7;
+        currentBoard[0, playerChoice] = playerTurn;
+    }        
     return currentBoard;
+}
+
+bool CheckForWin(char[, ] currentBoard)
+{
+    if (currentBoard[0, 0] == currentBoard[0, 1] && currentBoard[0, 1] == currentBoard[0, 2])
+    {
+        return true;
+    }
+    else if (currentBoard[1, 0] == currentBoard[1, 1] && currentBoard[1, 1] == currentBoard[1, 2])
+    {
+        return true;
+    }
+    else if (currentBoard[2, 0] == currentBoard[2, 1] && currentBoard[2, 1] == currentBoard[2, 2])
+    {
+        return true;
+    }
+    else if (currentBoard[0, 0] == currentBoard[1, 0] && currentBoard[1, 0] == currentBoard[2, 0])
+    {
+        return true;
+    }
+    else if (currentBoard[0, 1] == currentBoard[1, 1] && currentBoard[1, 1] == currentBoard[2, 1])
+    {
+        return true;
+    }
+    else if (currentBoard[0, 2] == currentBoard[1, 2] && currentBoard[1, 2] == currentBoard[2, 2])
+    {
+        return true;
+    }
+    else if (currentBoard[0, 0] == currentBoard[1, 1] && currentBoard[1, 1] == currentBoard[2, 2])
+    {
+        return true;
+    }
+    else if (currentBoard[2, 0] == currentBoard[1, 1] && currentBoard[1, 1] == currentBoard[0, 2])
+    {
+        return true;
+    }
+    else
+        return false;
 }

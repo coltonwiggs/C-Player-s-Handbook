796a1120c4c4ffedb0e3e0cb1e073cc16efaece7
coltonwiggs <90272823+coltonwiggs@users.noreply.github.com>
2026-08-25 21:27:06 -0500

    Add files via upload

---
+++ Packing Inventory/Packing Inventory/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+public class Pack
+{
+    private int _itemCount;
+    private double _weight;
+    private double _volume;
+    
+    public Pack(int itemCount, double weight, double volume)
+    {
+        _itemCount = itemCount;
+        _weight = weight;
+        _volume = volume;
+    }
+
+
+}
+
+public class InventoryItem
+{
+    private double _weight;
+    private double _volume;
+
+    public InventoryItem(double weight, double volume)
+    {
+        _weight = weight;
+        _volume = volume;
+    }
+
+
+}
+
+public class Arrow : InventoryItem
+{
+    public double weight;
+    public double volume;
+    
+    public Arrow()
+    {
+        weight = 0.1;
+    }
+}
